f86be75f76fc09ddf07aeffe50acdadef067b931
Jihad Ballout <148473573+yeezySznApproaching@users.noreply.github.com>
2024-11-18 14:56:30 +0100

    added 2 CRUD operations: get all items and get items by their IDs

--- simple-crud-api/app.js
+++ simple-crud-api/app.js
@@ -3,3 +3,20 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
+let items = [
+    {id: 1, name: 'Item 1', descrpition: 'This is Item 1'},
+    {id: 2, name: 'Item 2', descrpition: 'This is Item 2'}
+];
+
+app.get('/items', (req, res => {
+    res.json(items);
+}));
+
+app.get('/items/:id', (res,req => {
+    const item = items.find(i=> i.id === parseInt(req.params.id));
+    if (!item) return res.status(404).send('Item not found');
+    res.json(item);
+}));
+
