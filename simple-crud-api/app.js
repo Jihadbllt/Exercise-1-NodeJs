e6c66e52c6696c833fb1b7f74d31eb516adfd757
Jihad Ballout <148473573+yeezySznApproaching@users.noreply.github.com>
2024-11-18 15:02:43 +0100

    Created a new operation to edit items using PUT

--- simple-crud-api/app.js
+++ simple-crud-api/app.js
@@ -29,3 +29,16 @@ app.post('/items', (res, req => {
     items.push(newItem);
     res.status(201).json(newItem);
 }));
+
+app.put('/items/:id', (res, req =>{
+    const itemIndex = items.findIndex(i => i.id === parseInt(req.params.id));
+    if (itemIndex === -1) return res.status(404).send('Item not found');
+
+    items[itemIndex] = {
+        ...items[itemIndex],
+        name:req.body.name,
+        descrpition: req.body.descrpition
+    };
+    res.json(items[itemIndex]);
+}));
+
