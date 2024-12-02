7f0acca6b3549a85073bfe58aae0647a2fb1b26e
Jihad Ballout <148473573+yeezySznApproaching@users.noreply.github.com>
2024-12-02 14:19:53 +0100

    completed indexjs

--- simple-crud-api/src/index.js
+++ simple-crud-api/src/index.js
@@ -12,4 +12,27 @@ const databaseLogSubscriber = require ("./subscribers/databaseLogSubscriber");
 
 app.post("/", (req, res) => {
     const { name, createdAt} = req.body
+    if (!name || !createdAt) {
+        return res.status(400).json({message: "Name and createdAt are required"});
+    }
+    const newData = { name, createdAt}
+
+    console.log("Resource created: ", newData);
+
+    res.status(201).json({message: "Resource Created", data: newData});
+});
+
+app.get("/", (req, res) => {
+    db.all(`Select * FROM resources`, [], (err, rows) => {
+        if (err){
+            console.error("Error fetching resources:", err.message);
+            return res.status(500).json({message: "Error fetching resources"});
+        }
+        res.json({ data: rows});
+    })
+
+})
+
+app.listen(port, () => {
+    console.log(`Running here ... http://localhost:${port}/`);
 })
